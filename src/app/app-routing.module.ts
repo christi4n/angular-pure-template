269580ea3920f8b8415d159707b3ed0279f84f69
christian <androiddev99999@gmail.com>
2020-04-14 22:29:18 +0200

    [TASK] Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ import { ComponentsComponent } from './pages/components';
 
 const routes: Routes = [
   {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
     path: 'about',
     component: AboutComponent
   },
